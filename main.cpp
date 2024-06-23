f2aeeefac75d7c3f6c0d99a2d41e428a568a38f6
= <jamesjd1024@gmail.com>
2024-06-23 18:26:12 -0500

    made blank gui

--- main.cpp
+++ main.cpp
@@ -1,2 +1,87 @@
+#define GLFW_INCLUDE_NONE
 #include <cstdio>
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
+
+#define GL_ERROR_CASE(glerror)\
+    case glerror : snprintf(error, sizeof(error), "%s", #glerror)
+
+inline void glDebug(const char* file, int line) {
+    GLenum err;
+    while ((err = glGetError()) != GL_NO_ERROR) {
+        char error[128];
+
+        switch (err) {
+            GL_ERROR_CASE(GL_INVALID_ENUM); break;
+            GL_ERROR_CASE(GL_INVALID_VALUE); break;
+            GL_ERROR_CASE(GL_INVALID_OPERATION); break;
+            GL_ERROR_CASE(GL_INVALID_FRAMEBUFFER_OPERATION); break;
+            GL_ERROR_CASE(GL_OUT_OF_MEMORY); break;
+        
+        default: snprintf(error, sizeof(error), "%s", "UNKNOWN_ERROR");
+        }
+        fprintf(stderr, "%s - %s: %d\n", error, file, line);
+    }
+}
+
+#undef GL_ERROR_CASE
+
+void errorCallback(int error, const char* description) {
+    fprintf(stderr, "Error: %s\n", description);
+}
+
+
+int main(int argc, char* argv[]) {
+    glfwSetErrorCallback(errorCallback);
+
+    GLFWwindow* window;
+
+    if (!glfwInit()) {
+        return -1;
+    }
+
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    /* Create a windowed mode window with its OpenGL context */
+    window = glfwCreateWindow(640, 480, "James Space Invaders", NULL, NULL);
+    if (!window) {
+        fprintf(stderr, "Failed to create GLFW window\n");
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    /* Initialize GLEW */
+    GLenum glewError = glewInit();
+    if (glewError != GLEW_OK) {
+        fprintf(stderr, "Error initializing GLEW.\n");
+        glfwTerminate();
+        return -1;
+    }
+
+    int glVersion[2] = {-1, 1};
+    glGetIntegerv(GL_MAJOR_VERSION, &glVersion[0]);
+    glGetIntegerv(GL_MINOR_VERSION, &glVersion[1]);
+
+    // check for errors
+    glDebug(__FILE__, __LINE__);
+
+    printf("Using OpenGL: %d.%d\n", glVersion[0], glVersion[1]);
+    printf("Renderer used: %s\n", glGetString(GL_RENDERER));
+    printf("Shading Language: %s\n", glGetString(GL_SHADING_LANGUAGE_VERSION));
+
+    glClearColor(1.0, 0.0, 0.0, 1.0);
+    while (!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    return 0;
+}
